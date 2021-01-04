8ffafe74222099e8a662b9305461baf4e35a6a8f
ilmars <9987548+darksworm@users.noreply.github.com>
2021-01-04 22:33:50 +0200

    area and intersection methods, PolygonPiece & Board, Shape -> Polygon

--- polygon.py
+++ polygon.py
@@ -3,9 +3,11 @@ from typing import List
 
 import numpy
 
+PolygonPointsList = List[List[int]]
 
-class Shape:
-    def __init__(self, points: List[List[int]]):
+
+class Polygon:
+    def __init__(self, points: PolygonPointsList):
         self._points_matrix = numpy.array(points)
         self._move_to_quadrant_one()
 
@@ -35,7 +37,7 @@ class Shape:
         return self._get_point_sum() / [self._get_point_count(), self._get_point_count()]
 
     def _get_point_min(self) -> List[float]:
-        return self._points_matrix.min(axis=0, initial=None)
+        return self._points_matrix.min(axis=0, initial=None).tolist()
 
     def _move_to_quadrant_one(self) -> None:
         minimums = self._get_point_min()
@@ -71,3 +73,41 @@ class Shape:
     @staticmethod
     def _rotation_result_same_as_current_state(degrees) -> bool:
         return degrees % 360 == 0
+
+    def area(self) -> float:
+        x, y = zip(*self._points_matrix)
+        result = 0.5 * (numpy.dot(x, numpy.roll(y, 1)) - numpy.dot(y, numpy.roll(x, 1)))
+        return abs(result)
+
+
+class PolygonPiece:
+    def __init__(self, polygon: Polygon, position: List[int]):
+        self._polygon = polygon
+        self._position = position
+
+    def get_position(self):
+        return self._position
+
+    def get_polygon(self):
+        return self._polygon
+
+    def get_points_in_plane(self) -> numpy.ndarray:
+        return self._polygon.get_points() + self._position
+
+
+class Board(PolygonPiece):
+    def intersection_area(self, piece: PolygonPiece) -> float:
+        polygons_points = self._get_intersection_polygons_points(piece)
+        polygons = [Polygon(points) for points in polygons_points]
+        polygon_areas = [polygon.area() for polygon in polygons]
+        return sum(polygon_areas)
+
+    def _get_intersection_polygons_points(self, clip: PolygonPiece) -> List[PolygonPointsList]:
+        # don't import this globally to avoid use of it outside of this method
+        import pyclipper
+
+        clipper = pyclipper.Pyclipper()
+        clipper.AddPath(self.get_points_in_plane(), pyclipper.PT_SUBJECT, True)
+        clipper.AddPath(clip.get_points_in_plane(), pyclipper.PT_CLIP, True)
+
+        return clipper.Execute(pyclipper.CT_INTERSECTION, pyclipper.PFT_EVENODD, pyclipper.PFT_EVENODD)
--- test.py
+++ test.py
@@ -3,10 +3,10 @@ from random import randrange
 
 from parameterized import parameterized
 
-from shape import Shape
+from polygon import Polygon, Board, PolygonPiece
 
 
-class TestShape(unittest.TestCase):
+class TestPolygon(unittest.TestCase):
     def setUp(self) -> None:
         self.square_points = [
             [0, 0],
@@ -15,15 +15,15 @@ class TestShape(unittest.TestCase):
             [0, 1]
         ]
 
-    def test_shape_created_with_points_list(self):
-        Shape(self.square_points)
+    def test_polygon_created_with_points_list(self):
+        Polygon(self.square_points)
 
-    def test_shape_returns_points(self):
-        shape = Shape(self.square_points.copy())
-        self.assertEqual(shape.get_points().tolist(), self.square_points)
+    def test_polygon_returns_points(self):
+        polygon = Polygon(self.square_points.copy())
+        self.assertEqual(polygon.get_points().tolist(), self.square_points)
 
 
-class TestShapeRotations(unittest.TestCase):
+class TestPolygonRotations(unittest.TestCase):
     def setUp(self) -> None:
         self.square_points = [
             [0, 0],
@@ -43,19 +43,19 @@ class TestShapeRotations(unittest.TestCase):
         [0], [360], [-360], [720], [-5760]
     ])
     def test_rotate_360_doesnt_change_points(self, rotation):
-        shape = Shape(self.vertical_rectangle_points.copy())
-        shape.rotate(rotation)
-        rotated = [[round(p[0]), round(p[1])] for p in shape.get_points()]
+        polygon = Polygon(self.vertical_rectangle_points.copy())
+        polygon.rotate(rotation)
+        rotated = [[round(p[0]), round(p[1])] for p in polygon.get_points()]
         self.assertEqual(self.vertical_rectangle_points, rotated)
 
     @parameterized.expand([
         [randrange(-192834, 135783)] for _ in range(1, 300)
     ])
-    def test_shape_rotate_doesnt_result_in_negative_coords(self, rotation):
-        shape = Shape(self.vertical_rectangle_points.copy())
-        shape.rotate(rotation)
+    def test_polygon_rotate_doesnt_result_in_negative_coords(self, rotation):
+        polygon = Polygon(self.vertical_rectangle_points.copy())
+        polygon.rotate(rotation)
 
-        for point in shape.get_points():
+        for point in polygon.get_points():
             self.assertGreaterEqual(point[0], 0)
             self.assertGreaterEqual(point[1], 0)
 
@@ -63,9 +63,9 @@ class TestShapeRotations(unittest.TestCase):
         [90], [-90], [180], [360], [-180], [-360], [720], [-720]
     ])
     def test_rotate_square_has_same_points(self, rotation):
-        shape = Shape(self.square_points.copy())
-        shape.rotate(rotation)
-        rotated = [[round(p[0]), round(p[1])] for p in shape.get_points()]
+        polygon = Polygon(self.square_points.copy())
+        polygon.rotate(rotation)
+        rotated = [[round(p[0]), round(p[1])] for p in polygon.get_points()]
 
         for point in self.square_points:
             self.assertIn(point, rotated)
@@ -74,11 +74,11 @@ class TestShapeRotations(unittest.TestCase):
         [randrange(-192834, 135783)] for _ in range(1, 300)
     ])
     def test_rotate_and_rotate_back_returns_to_starting_points(self, rotation):
-        shape = Shape(self.square_points.copy())
-        shape.rotate(rotation)
-        shape.rotate(-rotation)
+        polygon = Polygon(self.square_points.copy())
+        polygon.rotate(rotation)
+        polygon.rotate(-rotation)
 
-        rotated = [[round(p[0]), round(p[1])] for p in shape.get_points()]
+        rotated = [[round(p[0]), round(p[1])] for p in polygon.get_points()]
 
         self.assertEqual(self.square_points, rotated)
 
@@ -95,7 +95,7 @@ def random_polygon_points(count: int):
     ]
 
 
-class TestShapeFlip(unittest.TestCase):
+class TestPolygonFlip(unittest.TestCase):
     def setUp(self) -> None:
         self.square_points = [
             [0, 0],
@@ -112,9 +112,9 @@ class TestShapeFlip(unittest.TestCase):
         ]
 
     def test_flip_square_has_same_points(self):
-        shape = Shape(self.square_points.copy())
-        shape.flip()
-        rotated = [[round(p[0]), round(p[1])] for p in shape.get_points()]
+        polygon = Polygon(self.square_points.copy())
+        polygon.flip()
+        rotated = [[round(p[0]), round(p[1])] for p in polygon.get_points()]
 
         for point in self.square_points:
             self.assertIn(point, rotated)
@@ -122,7 +122,7 @@ class TestShapeFlip(unittest.TestCase):
     @parameterized.expand(random_polygon_points(1))
     def test_flip_polygon_point_count_doesnt_change(self, *points):
         points = list(points)
-        poly = Shape(points.copy())
+        poly = Polygon(points.copy())
         poly.flip()
 
         self.assertEqual(len(points), len(poly.get_points()))
@@ -130,7 +130,7 @@ class TestShapeFlip(unittest.TestCase):
     @parameterized.expand(random_polygon_points(300))
     def test_flip_polygon_point_order_changes(self, *points):
         points = list(points)
-        poly = Shape(points.copy())
+        poly = Polygon(points.copy())
         poly.flip()
         flipped = [[round(p[0]), round(p[1])] for p in poly.get_points()]
 
@@ -138,28 +138,111 @@ class TestShapeFlip(unittest.TestCase):
 
     @parameterized.expand(random_polygon_points(300))
     def test_flip_twice_doesnt_change_surface_area(self, *points):
-        def poly_area(corners):
-            point_count = len(corners)
-            area = 0.0
-            for i in range(point_count):
-                j = (i + 1) % point_count
-                area += corners[i][0] * corners[j][1]
-                area -= corners[j][0] * corners[i][1]
-            area = abs(area) / 2.0
-            return area
-
         points = list(points)
-        poly = Shape(points.copy())
+        poly = Polygon(points.copy())
         poly.flip()
         poly.flip()
-        flipped = [[int(round(p[0])), int(round(p[1]))] for p in poly.get_points()]
 
-        self.assertEqual(poly_area(points), poly_area(flipped))
+        self.assertEqual(poly.area(), poly.area())
 
-    def test_shape_flip_doesnt_result_in_negative_coords(self):
-        shape = Shape(self.vertical_rectangle_points.copy())
-        shape.flip()
+    def test_polygon_flip_doesnt_result_in_negative_coords(self):
+        polygon = Polygon(self.vertical_rectangle_points.copy())
+        polygon.flip()
 
-        for point in shape.get_points():
+        for point in polygon.get_points():
             self.assertGreaterEqual(point[0], 0)
             self.assertGreaterEqual(point[1], 0)
+
+
+class TestPolygonPiece(unittest.TestCase):
+    def test_construct(self):
+        PolygonPiece(Polygon([[0, 0], [0, 10], [10, 0], [10, 10]]), [0, 0])
+
+    def test_get_position_returns_value_passed_in_construct(self):
+        position = [12, 543]
+        piece = PolygonPiece(Polygon([[0, 0], [0, 10], [10, 0], [10, 10]]), position.copy())
+        self.assertEqual(position, piece.get_position())
+
+    def test_polygon_used(self):
+        polygon = Polygon([[0, 0], [0, 10], [10, 0], [10, 10]])
+        piece = PolygonPiece(polygon, [1, 2])
+        self.assertEqual(polygon, piece.get_polygon())
+
+    @parameterized.expand(random_polygon_points(300))
+    def test_points_same_as_poly_if_position_zero(self, *points):
+        points = list(points)
+        polygon = Polygon(points.copy())
+        piece = PolygonPiece(polygon, [0, 0])
+        self.assertEqual(polygon.get_points().tolist(), piece.get_points_in_plane().tolist())
+
+    @parameterized.expand([
+        [randrange(-192834, 135783), randrange(-192834, 135783)] for _ in range(300)
+    ])
+    def test_points_not_same_as_poly_if_position_not_zero(self, *position):
+        position = list(position)
+        polygon = Polygon([[0, 0], [0, 10], [10, 0], [10, 10]])
+        piece = PolygonPiece(polygon, position)
+        self.assertNotEqual(polygon.get_points().tolist(), piece.get_points_in_plane().tolist())
+
+    @parameterized.expand([
+        [randrange(1, 135783), randrange(1, 135783)] for _ in range(300)
+    ])
+    def test_points_dont_decrease_with_positive_position(self, *position):
+        position = list(position)
+        polygon = Polygon([[0, 0], [0, 10], [10, 0], [10, 10]])
+        piece = PolygonPiece(polygon, position)
+        self.assertGreaterEqual(piece.get_points_in_plane().sum(), polygon.get_points().sum())
+
+    @parameterized.expand([
+        [randrange(-123123, -1), randrange(-123123, -1)] for _ in range(300)
+    ])
+    def test_points_dont_increase_with_positive_position(self, *position):
+        position = list(position)
+        polygon = Polygon([[0, 0], [0, 10], [10, 0], [10, 10]])
+        piece = PolygonPiece(polygon, position)
+        self.assertLessEqual(piece.get_points_in_plane().sum(), polygon.get_points().sum())
+
+
+class TestPolygonArea(unittest.TestCase):
+    def test_square_area_correct(self):
+        points = [[0, 0], [0, 1], [1, 1], [1, 0]]
+        expected_area = 1
+        poly = Polygon(points)
+        self.assertEqual(expected_area, poly.area())
+
+    def test_rectangle_area_correct(self):
+        points = [[0, 0], [0, 3], [1, 3], [1, 0]]
+        expected_area = 3
+        poly = Polygon(points)
+        self.assertEqual(expected_area, poly.area())
+
+    def test_right_angle_triangle_area_correct(self):
+        points = [[1, 1], [3, 3], [3, 1]]
+        expected_area = 2
+        poly = Polygon(points)
+        self.assertEqual(expected_area, poly.area())
+
+
+class TestGameBoard(unittest.TestCase):
+    def test_construct(self):
+        Board(Polygon([[0, 0], [0, 1000], [1000, 0], [1000, 1000]]), [0, 0])
+
+    def test_get_intersection_area_same_as_piece_if_fully_submerged(self):
+        board = Board(Polygon([[0, 0], [0, 1000], [1000, 1000], [1000, 0]]), [0, 0])
+        piece = PolygonPiece(Polygon([[0, 0], [100, 100], [100, 0]]), [50, 50])
+        self.assertEqual(piece.get_polygon().area(), board.intersection_area(piece))
+
+    def test_get_intersection_zero_if_poly_on_a_different_planet(self):
+        board = Board(Polygon([[0, 0], [0, 1000], [1000, 0], [1000, 1000]]), [0, 0])
+        piece = PolygonPiece(Polygon([[0, 0], [100, 100], [100, 0]]), [5000, 5000])
+        self.assertEqual(0, board.intersection_area(piece))
+
+    def test_get_intersection_area_same_if_poly_same_size(self):
+        board = Board(Polygon([[0, 0], [100, 100], [100, 0]]), [0, 0])
+        piece = PolygonPiece(Polygon([[0, 0], [100, 100], [100, 0]]), [0, 0])
+        self.assertEqual(piece.get_polygon().area(), board.intersection_area(piece))
+
+    def test_intersection_half_of_square(self):
+        board = Board(Polygon([[0, 0], [100, 100], [100, 0]]), [0, 0])
+        piece = PolygonPiece(Polygon([[0, 0], [0, 100], [100, 100], [100, 0]]), [0, 0])
+        self.assertEqual(piece.get_polygon().area() / 2, board.intersection_area(piece))
